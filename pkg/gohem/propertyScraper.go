6b73ac912d702a30ba948d9bd8f816b506de8b5b
zachath <zachariasthorell@gmail.com>
2023-09-16 08:29:02 +0200

    scrape a search

--- pkg/gohem/propertyScraper.go
+++ pkg/gohem/propertyScraper.go
@@ -4,28 +4,77 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/gocolly/colly/v2"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 )
 
+var bostadRegex = regexp.MustCompile(`^https:\/\/www\.hemnet\.se\/bostad\/\S*$`)
+
+func ScrapeSearch(url string) ([]Property, error) {
+	urls, err := collectUrls(url)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to collect urls")
+	}
+
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+	properties := &[]Property{}
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string, properties *[]Property) {
+			defer wg.Done()
+			property, err := ScrapeProperty(url)
+			if err != nil {
+				log.Error().Stack().Err(err).Msg("failed to scrape property")
+			} else {
+				mu.Lock()
+				*properties = append(*properties, property)
+				mu.Unlock()
+			}
+		}(url, properties)
+	}
+	wg.Wait()
+
+	return *properties, nil
+}
+
+func collectUrls(url string) ([]string, error) {
+	collector := colly.NewCollector()
+
+	urls := []string{}
+	collector.OnHTML("a", func(h *colly.HTMLElement) {
+		if bostadRegex.MatchString(h.Attr("href")) {
+			urls = append(urls, h.Attr("href"))
+		}
+	})
+
+	err := collector.Visit(url)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to visit url")
+	}
+
+	return urls, nil
+}
+
 func ScrapeProperty(url string) (Property, error) {
 	isRemoved, err := isPropertyRemoved(url)
 	if err != nil {
-		return Property{}, err
+		return Property{}, errors.WithMessage(err, "failed to determine if removed")
 	}
 
 	property := Property{}
 	if isRemoved {
 		property, err = scrapeRemoved(url)
 		if err != nil {
-			return Property{}, err
+			return Property{}, errors.WithMessage(err, "failed to scrape removed property")
 		}
 	} else {
 		property, err = scrape(url)
 		if err != nil {
-			return Property{}, err
+			return Property{}, errors.WithMessage(err, "failed to scrape property")
 		}
 	}
 
